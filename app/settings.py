b848d3519ec22109827a6fd8ae502787cef447f2
Dmitry Veko <dveko@ptsecurity.com>
2025-05-16 11:23:44 +0300

    feat: Поправил билдинг настроек

--- app/settings.py
+++ app/settings.py
@@ -108,12 +108,18 @@ class AppSettings(BaseSettings):  # noqa: WPS338
         credentials_str = credentials_str.replace("|", "@")
         assert credentials_str.count("@") == 2, "Have you forgot to add `bot_id`?"
 
-        host, secret_key, bot_id = [
+        cts_url, secret_key, bot_id = [
             str_value.strip() for str_value in credentials_str.split("@")
         ]
+
+        if "://" not in cts_url:
+            cts_url = f"https://{cts_url}"
+
         return BotAccountWithSecret(
-            id=UUID(bot_id), host=host, secret_key=secret_key
-        )  # type: ignore[call-arg]
+            id=UUID(bot_id),
+            cts_url=cts_url,  # type: ignore[arg-type]
+            secret_key=secret_key,
+        )
 
     @validator("BOT_CREDENTIALS", pre=True)
     @classmethod
