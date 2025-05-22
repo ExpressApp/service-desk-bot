4cdf3c083579979a00ae853c743127e3a2984218
Dmitry Veko <dveko@ptsecurity.com>
2025-05-22 13:17:52 +0300

    feat: Убрал дублирующую VERIFY_SSL переменную VERIFY_REQUEST

--- app/api/endpoints/botx.py
+++ app/api/endpoints/botx.py
@@ -29,6 +29,7 @@ async def command_handler(request: Request, bot: Bot = bot_dependency) -> JSONRe
         bot.async_execute_raw_bot_command(
             await request.json(),
             request_headers=request.headers,
+            verify_request=settings.VERIFY_SSL
         )
     except ValueError:
         error_label = "Bot command validation error"
@@ -75,6 +76,7 @@ async def status_handler(request: Request, bot: Bot = bot_dependency) -> JSONRes
         status = await bot.raw_get_status(
             dict(request.query_params),
             request_headers=request.headers,
+            verify_request=settings.VERIFY_SSL
         )
     except UnknownBotAccountError as exc:
         error_label = f"Unknown bot_id: {exc.bot_id}"
@@ -101,6 +103,7 @@ async def callback_handler(request: Request, bot: Bot = bot_dependency) -> JSONR
         await bot.set_raw_botx_method_result(
             await request.json(),
             request_headers=request.headers,
+            verify_request=settings.VERIFY_SSL
         )
     except BotXMethodCallbackNotFoundError as exc:
         error_label = f"Unexpected callback with sync_id: {exc.sync_id}"
